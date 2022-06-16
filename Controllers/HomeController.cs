4da3f5dd9b2500199e604768477fa903e306ac40
markoloiola <markoloiola@gmail.com>
2022-06-15 21:43:22 -0300

    Finalizado explicação do professor referente ao tipos de requisições Http's

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,10 +3,41 @@ using Microsoft.AspNetCore.Mvc;
 namespace PrimeiraAPI7P.Controllers;
 [ApiController] //Anotação
 [Route("[controller]")]
-public class HomeController: ControllerBase
+public class HomeController : ControllerBase
 {
     [HttpGet] //informando que vai ser um requisição do tipo GET
-    public string GetMensagem(){
+    public string GetMensagem()
+    {
         return "Olá Mundo";
+    }
+
+    // [HttpGet] //Testado duas requisições de tipos iguais e constado erro
+    // public string Getmensagem2()
+    // {
+    //     return "Olá Mundo 2";
+    // }
+
+    [HttpGet("index")]
+    public string GetIndex()
+    {
+        return "Rota Index";
+    }
+
+    [HttpPost]
+    public object PostObjeto([FromBody] object mensagem)
+    {
+        return mensagem;
+    }
+
+    [HttpPut]
+    public object PutObjeto([FromBody] object objeto)
+    {
+        return objeto;
+    }
+
+    [HttpDelete]
+    public string delete()
+    {
+        return "Deletedo com sucesso!";
     }
 }
